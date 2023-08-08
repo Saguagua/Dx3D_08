9b9bb4ca2c9406f02ef4a355ebe73b44d93518ea
Saguagua <93913569+Saguagua@users.noreply.github.com>
2023-08-08 15:00:12 +0900

    dx 초기화 중

---
+++ DirectX3D/DX3DFirst/Framework.h
@@ -0,0 +1,24 @@
+﻿// header.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "__Others/targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#include <windows.h>
+
+#include <d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+#define WIN_WIDTH 1280.0f
+#define	WIN_HEIGHT 720.0f
+
+//UI 사용자가 컴퓨터에 명령을 내릴 수 있게 하는 것
+
+//DX 그래픽과 스피드 상승을 위해 등장
+
+//컴파일 단계에서 cpp을 읽는다. h는 읽지 않는다
+//인라인 함수 = 메크로와 같이 전처리 단계에서 치환
---
+++ DirectX3D/DX3DFirst/Main.cpp
@@ -0,0 +1,249 @@
+﻿// DX3DFirst.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "Framework.h"
+#include "Main.h"
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+ID3D11Device*           _device;                        // CPU를 다루는 객체, 무언가를 만들 때
+ID3D11DeviceContext*    _deviceContext;                 // GPU를 다루는 객체, 무언가를 그릴 때
+
+IDXGISwapChain* _swapChain;                             //더블버퍼를 구현하는 방식 중 하나
+ID3D11RenderTargetView* _renderTargetView;              //view가 들어가면 전부 GPU 관련, 백버퍼를 관리한다. PIP(picture in picture) 화면안에 화면 보여주는 것
+                                                        //미니맵, 렌더타겟은 카메라 1개와 같다. 총 8개까지 가능하다
+
+HWND hWnd;
+HINSTANCE hInst;                                        // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                          // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];                    // 기본 창 클래스 이름입니다.
+
+void Initialize();
+void Release();
+void Render();
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DX3DFIRST, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DX3DFIRST));
+
+    MSG msg;
+    Initialize();
+
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            Render();
+        }
+    }
+
+    Release();
+
+    return (int) msg.wParam;
+}
+
+
+
+void Initialize()
+{
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+    swapChainDesc.BufferCount = 1;
+    swapChainDesc.BufferDesc.Width = WIN_WIDTH;
+    swapChainDesc.BufferDesc.Height = WIN_HEIGHT;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; //RGBA 8비트 4개, unorm = unsigned normal = 0 ~ 1
+    
+    swapChainDesc.BufferDesc.RefreshRate.Numerator      = 60;
+    swapChainDesc.BufferDesc.RefreshRate.Denominator    = 1;
+
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.OutputWindow = hWnd;
+
+    //sampling : 표본화. 확대, 축소 연산의 값을 미리 계산해 도움을 준다. 계단현상을 없애준다.
+    swapChainDesc.SampleDesc.Count = 1;     //주변 픽셀 몇 개
+    swapChainDesc.SampleDesc.Quality = 0;   //범위는 얼마나
+    //AA : 안티엘리어싱 
+    //엘리어싱 : 계단현상
+
+    swapChainDesc.Windowed = true;
+}
+
+void Release()
+{
+}
+
+void Render()
+{
+}
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DX3DFIRST));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DX3DFIRST);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   RECT rect = {0,0,WIN_WIDTH,WIN_HEIGHT};
+   
+   AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+   hWnd = CreateWindowW(
+       szWindowClass, 
+       szTitle,
+       WS_OVERLAPPEDWINDOW,
+       CW_USEDEFAULT, 
+       0, 
+       rect.right - rect.left,
+       rect.bottom - rect.top, 
+       nullptr, nullptr, hInstance, nullptr
+   );
+
+   SetMenu(hWnd, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
